81c8be04337f9cd9c658f72465ae09da570e9204
ferys2195 <fery21irawan.me@gmail.com>
2025-11-19 09:34:47 +0700

    feat : add admin menu template

--- apps/frontend-v2/src/App.tsx
+++ apps/frontend-v2/src/App.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
+import IndexPage from "./pages";
 
 function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  );
+  return <IndexPage />;
 }
 
 export default App;
---
+++ apps/frontend-v2/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import AppLayout from "@/layouts/AppLayout";
+
+export default function IndexPage() {
+  return (
+    <AppLayout>
+      <p>Oy Aku</p>
+    </AppLayout>
+  );
+}
